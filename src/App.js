98a8483dceb40e7d0903afa3b1c6582e81d87d87
Tommy <spiel.hoelle@live.de>
2018-04-26 10:30:09 +0200

    write into state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    form: {},
+    items: []
+  }
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+    var form = this.state.form;
+    var items = this.state.items;
+
+    items.push(form)
+    this.setState({items})
+  }
+  onChange = (e) => {
+    console.log(e.target.value);
+    this.setState({form: e.target.value})
+    console.log(this);
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +31,13 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <form onSubmit={(e) => this.onSubmit(e)}>
+          <label>
+            Name:
+            <input onChange={(e) => this.onChange(e)} type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
